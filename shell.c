c290f5751359ce71efbcb2c7c5da677c87db3748
emili-michael <emili-michael@spring2024-iit.kdlp.underground.software>
2024-06-13 22:13:31 +0300

    lvl 2: shell prints "Unrecognized command"
    If the user types any text, the shell prints "Unrecognized command"
    and stays in loop. However, if the user just hits enter without
    typing anything, no error message is printed.
    
    Signed-off by: Emily Dror

--- shell.c
+++ shell.c
@@ -12,11 +12,15 @@ int main(void)
         free(cwd);
 
         // Read command from user
-        char*  line = NULL;
-        size_t len  = 0;
-        getline(&line, &len, stdin);
+        char*   line     = NULL;
+        size_t  len      = 0;
+        ssize_t line_sz  = getline(&line, &len, stdin);
+
+        if (line_sz > 1) {
+            puts("Unrecognized command");
+        }
 
     } while (!feof(stdin));
-    printf("\n");
+    puts("\n");
     return 0;
 }
